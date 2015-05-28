964d5df71b02f5a91d6920a7dec5d4f2ff609937
Adam Bornak <adam.bornak@gmail.com>
2015-05-28 13:10:57 +0200

    first commit v1

---
+++ product-manager/api/v1/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'angularcode_products');
+
+?>
